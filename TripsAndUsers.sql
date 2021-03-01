5a292166cf964055a94fc7c896df63c98050cd1b
yueqin27 <yueqin27@users.noreply.github.com>
2021-03-01 15:40:13 -0800

    Update TripsAndUsers.sql

--- TripsAndUsers.sql
+++ TripsAndUsers.sql
@@ -1,9 +1,7 @@
 '''
-Write a SQL query to find the cancellation rate of requests with unbanned users (both client and driver must not be banned) each day between "2013-10-01" and "2013-10-03".
+Write a SQL query to find the cancellation rate of requests with unbanned users each day between "2013-10-01" and "2013-10-03".
 
-The cancellation rate is computed by dividing the number of canceled (by client or driver) requests with unbanned users by the total number of requests with unbanned users on that day.
-
-Return the result table in any order. Round Cancellation Rate to two decimal points.
+The cancellation rate is computed by dividing the number of canceled requests with unbanned users by the total number of requests with unbanned users on that day.
 '''
 
 
