c1571577235962c9c5c0f76820d7b57015b2dcc1
yueqin27 <yueqin27@users.noreply.github.com>
2021-03-01 15:41:08 -0800

    Update SalesAnalysis.sql

--- SalesAnalysis.sql
+++ SalesAnalysis.sql
@@ -5,15 +5,15 @@ Write an SQL query that reports the products that were only sold in spring 2019.
 select product_id, product_name
 from Product
 where product_id in 
-(SELECT t1.product_id
-FROM 
+(select t1.product_id
+from 
 (select product_id
 from Sales 
 where sale_date >= "2019-01-01" and sale_date <= "2019-03-31") t1
-LEFT JOIN 
+LEFT join 
 (select product_id
 from Sales 
 where sale_date < "2019-01-01" or sale_date > "2019-03-31") t2 
 ON t2.product_id = t1.product_id
-WHERE t2.product_id IS NULL
+where t2.product_id is NULL
 )
